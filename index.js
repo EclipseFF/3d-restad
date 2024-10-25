8b95d0b7a136403ab536d419e3dc020dec8505ed
EclipseFF <uevisveh9174@gmail.com>
2024-10-25 05:39:36 +0100

    123

--- index.js
+++
@@ -1,8 +0,0 @@
-AFRAME.registerComponent('model', {
-    init: function () {
-        this.el.addEventListener('model-loaded', function (event) {
-            console.log(event.detail);
-            console.log(213);
-        });
-    }
-});
